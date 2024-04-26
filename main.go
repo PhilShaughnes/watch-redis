6113cd145cbd3b0a1a9c9264cc78da07d686bbad
Phil Shaughnessy <phil.shaughnessy@greenlight.guru>
2024-04-26 10:12:49 -0400

    Adds duration

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log/slog"
+	"strconv"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -14,7 +15,7 @@ func main() {
 	var (
 		rhost = flag.String("host", "localhost", "redis server host")
 		rport = flag.Int("port", 6379, "redis server port")
-		key   = flag.String("key", "seed:yell_e2e_three", "redis key to watch")
+		key   = flag.String("key", "namespace:key", "redis key to watch")
 	)
 	flag.Parse()
 
@@ -27,14 +28,25 @@ func main() {
 	for {
 		res, err := rdb.Get(ctx, *key).Result()
 		if value != res {
-			handleChange(*key, res, err)
+			handleChange(*key, res, value, err)
 		}
 		value = res
 		time.Sleep(time.Millisecond * 200)
 	}
 }
 
-func handleChange(redisKey string, redisVal string, err error) {
+func timeSince(unix string) time.Duration {
+	unixTimestamp, err := strconv.ParseInt(unix, 10, 64)
+	if err != nil {
+		slog.Error("couldn't convert to unix timestamp")
+	}
+	t := time.Unix(unixTimestamp/1000, 0)
+	duration := time.Since(t)
+	return duration
+}
+
+func handleChange(redisKey, redisVal, oldVal string, err error) {
+	var duration time.Duration
 	if err != nil && err != redis.Nil {
 		slog.Error(
 			"couldn't retrieve",
@@ -43,5 +55,10 @@ func handleChange(redisKey string, redisVal string, err error) {
 		)
 		return
 	}
-	slog.Info(fmt.Sprintf("%s::%s", redisKey, redisVal))
+
+	if oldVal != "" {
+		duration = timeSince(oldVal)
+	}
+
+	slog.Info(fmt.Sprintf("%s::%s [%v]", redisKey, redisVal, duration))
 }
